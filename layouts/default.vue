62a0031db7a3c43e53b478e81c0c6b249fe64fba
cyber-developer <cyber.devworld@gmail.com>
2019-01-17 21:02:13 +0500

    Multiple files - basic layout and structural changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,87 +1,15 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon" />
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"/>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer
-      :right="right"
-      v-model="rightDrawer"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2017</span>
-    </v-footer>
+  <v-app>
+    <v-layout column>
+      <v-content>
+        <v-container style="height: 100%">
+          <nuxt />
+        </v-container>
+      </v-content>
+      <v-footer app>
+        <span>&copy; all rights reserved 2019</span>
+      </v-footer>
+    </v-layout>
   </v-app>
 </template>
 
@@ -89,18 +17,39 @@
   export default {
     data() {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
       }
     }
   }
 </script>
+
+<style>
+  #app {
+    color: rgb(65, 65, 65);
+  }
+  .h-100 {
+    height: 100%;
+  }
+  .align-right {
+    text-align: right;
+  }
+  .w-fit-content {
+    width: fit-content;
+    width: -moz-fit-content;
+  }
+  .color-gray {
+    color: rgb(65, 65, 65);
+  }
+  .link-undecorated {
+    color: rgb(65, 65, 65);
+    text-decoration: none;
+  }
+  .row-flex {
+    display: flex;
+    flex-direction: row;
+  }
+  .col-flex {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
